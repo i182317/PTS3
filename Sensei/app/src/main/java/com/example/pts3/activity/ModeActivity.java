387ed34f2390f9effbd22b846aa838f35348e648
-v <nathanlefort53@gmail.com>
2019-12-10 09:04:15 +0100

    commencer mode activity

---
+++ Sensei/app/src/main/java/com/example/pts3/activity/ModeActivity.java
@@ -0,0 +1,16 @@
+package com.example.pts3.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.pts3.R;
+
+public class ModeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mode_activity);
+    }
+}
