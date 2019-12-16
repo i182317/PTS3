d8412986c67640292daa39c6fbccfd1664171b66
-v <nathanlefort53@gmail.com>
2019-12-16 07:53:11 +0100

    activity choix plateau v1 fini

--- Sensei/app/src/main/java/com/example/pts3/activity/Plateau_Activity.java
+++ Sensei/app/src/main/java/com/example/pts3/activity/Plateau_Activity.java
@@ -2,17 +2,20 @@ package com.example.pts3.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.example.pts3.R;
+import com.example.pts3.classes.Plateau;
 import com.example.pts3.classes.XmlEditor;
 
 public class Plateau_Activity extends AppCompatActivity {
 
-    private int gameMode;           // Mode de jeu selectionné : 1 = Arashi et 2 = Shizukana
-    private XmlEditor xmlEditor;    // Permet de gérer le fichier xml des platreau
-    private List<> tabPlateau;
+    private Plateau_Activity plateau_Activity;  // Activity courrante
+    private int gameMode;                       // Mode de jeu selectionné : 1 = Arashi et 2 = Shizukana
+    private Button buttonPlateau1;              // Boutton du plateau 1
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,11 +26,19 @@ public class Plateau_Activity extends AppCompatActivity {
         Bundle extras = getIntent().getExtras();
         this.gameMode = extras.getInt("gameMode");
 
-        // Gestion du fichier XML
-        this.xmlEditor = new XmlEditor();
+        // Initialisation du bouttonPlateau1
+        this.plateau_Activity = this;
+        this.buttonPlateau1 = findViewById(R.id.buttonPlateau1);
+        this.buttonPlateau1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(plateau_Activity, Game_Activity.class);
+                intent.putExtra("gameMode", gameMode);
+                plateau_Activity.startActivity(intent);
+                plateau_Activity.finish();
+            }
+        });
 
-        // Lecture du fichier XML
-        this.xmlEditor.read();
     }
 
     // Mise en plein écran :
